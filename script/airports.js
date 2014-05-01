4416e347faa928fa952595f4609fb9143e6ddbd7
danasilver <dsilver1221@gmail.com>
2014-05-01 03:25:41 -0400

    Initial mapping with airports.

--- script/airports.js
+++ script/airports.js
@@ -8,7 +8,18 @@ var airportsFilePath = path.resolve(__dirname, '../data/airports/airports-clean.
   , airportsContents = fs.readFileSync(airportsFilePath, 'utf8')
   , parsedAirports = d3.csv.parse(airportsContents);
 
-parsedAirports.forEach(function(d) {
+var states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA",
+              "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD",
+              "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ",
+              "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC",
+              "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY", "DC"];
+
+parsedAirports = parsedAirports.filter(function(d) {
+  for (var i = 0; i < states.length; i++) if (d.State === states[i]) return true;
+  return false;
+});
+
+parsedAirports.forEach(function(d, i) {
   d.latitude = degreesToDecimal(d.ARPLatitude);
   d.longitude = degreesToDecimal(d.ARPLongitude);
 });
@@ -17,7 +28,10 @@ fs.writeFileSync(path.resolve(__dirname, '../data/airports/airports.json'), JSON
 
 function degreesToDecimal(degrees) {
   var parts = degrees.split('-');
+  parts[3] = parts[2].substr(7, 1);
   parts[2] = parts[2].substr(0, 7);
 
-  return +parts[0] + (+parts[1] / 60) + (+parts[2] / 3600);
+  var unsigned = +parts[0] + (+parts[1] / 60) + (+parts[2] / 3600);
+
+  return (parts[3] === 'W' || parts[3] === 'S') ? 0 - unsigned : unsigned;
 }
